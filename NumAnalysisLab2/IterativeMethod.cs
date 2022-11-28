9554094503f0d62648351ddfd16c5615ce003dd3
romardoss <romassuper@gmail.com>
2022-11-28 19:26:39 +0200

    First test added

---
+++ NumAnalysisLab2/IterativeMethod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NumAnalysisLab2
+{
+    internal class IterativeMethod
+    {
+        public bool IsAbleToCalculate(double[,] m)
+        {
+            int n = m.GetLength(0);
+            for (int i = 0; i < n; i++)
+            {
+                double sum = 0;
+                for(int j = 0; j < n+1; j++)
+                {
+                    sum += m[i, j];
+                }
+                sum -= m[i, i];
+                if(m[i, i] <= sum)
+                {
+                    Console.WriteLine("Вiдсутня дiагональна перевага");
+                    Console.WriteLine("Неможливо обрахувати цю матрицю iтерацiйними методами");
+                    Console.WriteLine("Перетворiть її на матрицю з дiагональною перевагою");
+                    return false;
+                }
+            }
+            Console.WriteLine("Дiагональна перевага присутня");
+            Console.WriteLine("Матрицю можна обраховувати iтерацiйними методами");
+            return true;
+        }
+
+    }
+}
---
+++ NumAnalysisLab2/Main_Lab2.cs
@@ -0,0 +1,30 @@
+﻿namespace NumAnalysisLab2
+{
+    internal class Main_Lab2
+    {
+        static readonly int variant = 6;
+        static readonly int k = variant - 5;
+        static readonly double a = 0.5 * k;
+        static readonly double b = 0.5 * k;
+
+        static double[,] Matrix = {
+            {3.81, 0.25, 1.28, 0.75+a, 4.21   },
+            {2.25, 1.32, 4.58+a, 0.49, 6.47+b },
+            {5.31, 6.28+a, 0.98, 1.04, 2.38   },
+            {9.39+a, 2.45, 3.35, 2.28, 10.48+b}
+        };
+
+        static double[,] MatrixTest = {
+            {100, 0.25, 1.28, 0.75+a, 4.21   },
+            {2.25, 100, 4.58+a, 0.49, 6.47+b },
+            {5.31, 6.28+a,100, 1.04, 2.38   },
+            {9.39+a, 2.45, 3.35, 100, 10.48+b}
+        };
+
+        static void Main(string[] args)
+        {
+            IterativeMethod i = new();
+            i.IsAbleToCalculate(MatrixTest);
+        }
+    }
+}
