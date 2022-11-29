7b6e9bd9c0d926be67fc916b9e09d8ed5beb30d6
romardoss <romassuper@gmail.com>
2022-11-29 13:24:35 +0200

    Two methods added

--- NumAnalysisLab2/IterativeMethod.cs
+++ NumAnalysisLab2/IterativeMethod.cs
@@ -4,7 +4,13 @@ namespace NumAnalysisLab2
 {
     internal class IterativeMethod
     {
-        public bool IsAbleToCalculate(double[,] m)
+        public static double[] ValueOfArguments = { 0, 0, 0, 0 };
+        //вказує на значення х1, х2, х3, х4
+        public static double[] ValueOfArgumentsTemp = { 0, 0, 0, 0 };
+        //тимчасові значення іксів, зберігаються до кінця однієї ітерації
+        //а потім запишуться у ValueOfArguments
+
+        public static bool IsAbleToCalculate(double[,] m)
         {
             int n = m.GetLength(0);
             for (int i = 0; i < n; i++)
@@ -12,10 +18,10 @@ namespace NumAnalysisLab2
                 double sum = 0;
                 for(int j = 0; j < n+1; j++)
                 {
-                    sum += m[i, j];
+                    sum += Math.Abs(m[i, j]);
                 }
-                sum -= m[i, i];
-                if(m[i, i] <= sum)
+                sum -= Math.Abs(m[i, i]);
+                if(Math.Abs(m[i, i]) <= sum)
                 {
                     Console.WriteLine("Вiдсутня дiагональна перевага");
                     Console.WriteLine("Неможливо обрахувати цю матрицю iтерацiйними методами");
@@ -26,6 +32,56 @@ namespace NumAnalysisLab2
             Console.WriteLine("Дiагональна перевага присутня");
             Console.WriteLine("Матрицю можна обраховувати iтерацiйними методами");
             return true;
+        }
+
+        public static void StartOfWork(double[,] m)
+        {
+            //Тут відбуватиметься перестановка елементів з вигляду 
+            //x1+x2+x3=b до вигляду x1=b-x2-x3 і так само для інших іксів в наступних рядках
+            int n = m.GetLength(0);
+            for (int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    m[i, j] *= -1;
+                }
+                double temp = m[i, 0];
+                m[i, 0] = m[i, i] * -1;
+                if(i != 0)
+                    m[i, i] = temp;
+            }
+            //x1 = -x2 -x3 -x4 b
+            //x2 = -x1 -x3 -x4 b etc
+
+            for (int i = 0; i < n; i++)
+            {
+                double divider = m[i, 0];
+                for(int j = 0; j < n+1; j++)
+                {
+                    m[i, j] /= divider;
+                }
+            }
+        }
+
+        public static void CalculateOneIteration(double[,] m)
+        {
+            int n = m.GetLength(0);
+            for (int i = 0; i < n; i++)
+            {
+                double sum = 0;
+                for (int j = 1; j < n; j++)
+                {
+                    int IndexOfCurrentX = j;
+                    if (j == i) j++;
+                    sum += m[i, j] * ValueOfArguments[IndexOfCurrentX];
+                }
+                sum += m[i, n];
+                ValueOfArgumentsTemp[i] = sum;
+            }
+            for(int i = 0; i < ValueOfArguments.Length; i++)
+            {
+                ValueOfArguments[i] = ValueOfArgumentsTemp[i];
+            }
         }
 
     }
--- NumAnalysisLab2/Main_Lab2.cs
+++ NumAnalysisLab2/Main_Lab2.cs
@@ -1,4 +1,6 @@
-﻿namespace NumAnalysisLab2
+﻿using System;
+
+namespace NumAnalysisLab2
 {
     internal class Main_Lab2
     {
@@ -14,17 +16,34 @@
             {9.39+a, 2.45, 3.35, 2.28, 10.48+b}
         };
 
-        static double[,] MatrixTest = {
-            {100, 0.25, 1.28, 0.75+a, 4.21   },
-            {2.25, 100, 4.58+a, 0.49, 6.47+b },
-            {5.31, 6.28+a,100, 1.04, 2.38   },
-            {9.39+a, 2.45, 3.35, 100, 10.48+b}
-        };
-
         static void Main(string[] args)
         {
-            IterativeMethod i = new();
-            i.IsAbleToCalculate(MatrixTest);
+            IterativeMethod.IsAbleToCalculate(Matrix);
+            Console.WriteLine();
+            IterativeMethod.StartOfWork(Matrix);
+            IterativeMethod.CalculateOneIteration(Matrix);
+            PrintMatrix(Matrix);
+            Console.WriteLine();
+            double[] array = IterativeMethod.ValueOfArguments;
+            foreach (double i in array)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        static void PrintMatrix(double[,] m)
+        {
+            int rowLength = m.GetLength(0);
+            int colLength = m.GetLength(1);
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    Console.Write(string.Format("{0} ", m[i, j]));
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
